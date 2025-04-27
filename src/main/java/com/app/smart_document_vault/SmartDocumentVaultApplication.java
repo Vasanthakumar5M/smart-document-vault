c4d7d09f16896b0974c1aa8005936a1ead783ff5
Vasanthakumar5M <mvasanthakumar381@gmail.com>
2025-04-27 13:15:58 +0530

    adding additional properties

--- src/main/java/com/app/smart_document_vault/SmartDocumentVaultApplication.java
+++ src/main/java/com/app/smart_document_vault/SmartDocumentVaultApplication.java
@@ -2,10 +2,12 @@ package com.app.smart_document_vault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@PropertySource("classpath:additional.properties")
 public class SmartDocumentVaultApplication {
 
 	public static void main(String[] args) {
